ab4c532f41e7e11b42b2224278bb924474d4c4f8
Andrew Kim <drewkenjo@gmail.com>
2016-11-29 03:31:40 -0500

    test ignoring

--- src/main/java/org/jlab/analysis/eg1dvcs/kenjo/EventSelector.java
+++ src/main/java/org/jlab/analysis/eg1dvcs/kenjo/EventSelector.java
@@ -11,72 +11,76 @@ import org.jlab.clas.physics.Particle;
 import org.jlab.clas.physics.PhysicsEvent;
 import org.jlab.io.base.DataEvent;
 import org.jlab.io.evio.EvioDataBank;
+
 /**
  *
  * @author kenjo
  */
 public class EventSelector extends GenericKinematicFitter {
+
     protected final Double mybeam;
+
     public EventSelector(double beam) {
         super(beam);
         mybeam = beam;
     }
-    
-        /**
+
+    /**
      * Returns PhysicsEvent object with reconstructed particles.
+     *
      * @param event - DataEvent object
      * @return PhysicsEvent : event containing particles.
      */
     @Override
-    public PhysicsEvent  getPhysicsEvent(DataEvent  event){
+    public PhysicsEvent getPhysicsEvent(DataEvent event) {
         //if(event instanceof EvioDataEvent){
-            //System.out.println("   CHECK FOR  PARTICLE = " + event.hasBank("EVENT::particle"));
-        if(event.hasBank("EVENT::particle")){
+        //System.out.println("   CHECK FOR  PARTICLE = " + event.hasBank("EVENT::particle"));
+        if (event.hasBank("EVENT::particle")) {
             EvioDataBank evntBank = (EvioDataBank) event.getBank("EVENT::particle");
             int nrows = evntBank.rows();
-            PhysicsEvent  physEvent = new PhysicsEvent();
-                physEvent.setBeam(this.mybeam);
-                for(int loop = 0; loop < nrows; loop++){
+            PhysicsEvent physEvent = new PhysicsEvent();
+            physEvent.setBeam(this.mybeam);
+            for (int loop = 0; loop < nrows; loop++) {
 
-                    int pid    = evntBank.getInt("pid", loop);
-                    int status = evntBank.getByte("status", loop);
-                    int dcstat = evntBank.getByte("dcstat", loop);
-                    int scstat = evntBank.getByte("scstat", loop);
-                    if(PDGDatabase.isValidPid(pid)==true){
-                        Particle part = new Particle(
-                                evntBank.getInt("pid", loop),
-                                evntBank.getFloat("px", loop),
-                                evntBank.getFloat("py", loop),
-                                evntBank.getFloat("pz", loop),
-                                evntBank.getFloat("vx", loop),
-                                evntBank.getFloat("vy", loop),
-                                evntBank.getFloat("vz", loop));
-                        if(status>0){
-                            physEvent.addParticle(part);
-                        }
-                    } else {
-                        Particle part = new Particle();
-                        part.setParticleWithMass(evntBank.getFloat("mass", loop),
-                                evntBank.getByte("charge", loop),
-                                evntBank.getFloat("px", loop),
-                                evntBank.getFloat("py", loop),
-                                evntBank.getFloat("pz", loop),
-                                evntBank.getFloat("vx", loop),
-                                evntBank.getFloat("vy", loop),
-                                evntBank.getFloat("vz", loop)
-                        );
-                        
-                        if(status>0){
-                            physEvent.addParticle(part);
-                        }
+                int pid = evntBank.getInt("pid", loop);
+                int status = evntBank.getByte("status", loop);
+                int dcstat = evntBank.getByte("dcstat", loop);
+                int scstat = evntBank.getByte("scstat", loop);
+                if (PDGDatabase.isValidPid(pid) == true) {
+                    Particle part = new Particle(
+                            evntBank.getInt("pid", loop),
+                            evntBank.getFloat("px", loop),
+                            evntBank.getFloat("py", loop),
+                            evntBank.getFloat("pz", loop),
+                            evntBank.getFloat("vx", loop),
+                            evntBank.getFloat("vy", loop),
+                            evntBank.getFloat("vz", loop));
+                    if (status > 0) {
+                        physEvent.addParticle(part);
                     }
-                   
+                } else {
+                    Particle part = new Particle();
+                    part.setParticleWithMass(evntBank.getFloat("mass", loop),
+                            evntBank.getByte("charge", loop),
+                            evntBank.getFloat("px", loop),
+                            evntBank.getFloat("py", loop),
+                            evntBank.getFloat("pz", loop),
+                            evntBank.getFloat("vx", loop),
+                            evntBank.getFloat("vy", loop),
+                            evntBank.getFloat("vz", loop)
+                    );
+
+                    if (status > 0) {
+                        physEvent.addParticle(part);
+                    }
                 }
-                return physEvent;
+
             }
-            
+            return physEvent;
+        }
+
         //}
         return new PhysicsEvent(this.mybeam);
     }
-    
+
 }
